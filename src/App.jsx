ed0e278802e85a5b3f94b80b8f88ca7465f0aff7
novaeeken <nova_eeken@hotmail.com>
2025-01-31 13:29:59 +0100

    Finish opdacht 2 (deel 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import './App.css';
-import { inventory} from "./constants/inventory.js";
+import {bestSellingTv, inventory} from "./constants/inventory.js";
 import calculateSoldProducts from "./helpers/calculateSoldProducts.js";
 import calculateInitialStock from "./helpers/calculateInitialStock.js";
 import calculateProductsToSell from "./helpers/calculateProductsToSell.js";
+import createProductName from './helpers/createProductName.js';
+import createProductPrice from './helpers/createProductPrice.js';
+import createScreenSizesString from './helpers/createScreenSizesString.js';
+import minus from './assets/minus.png';
+import check from './assets/check.png';
+import './App.css';
 
 function App() {
     return (
@@ -24,6 +29,26 @@ function App() {
                         <h2>{calculateProductsToSell(inventory)}</h2>
                     </article>
                 </div>
+            </section>
+            <section className="best-seller-container">
+                <h2>Best verkochte tv</h2>
+                <article className="product product-best-seller">
+                    <span className="product-image">
+                        <img src={bestSellingTv.sourceImg} alt="Afbeelding van het product"/>
+                    </span>
+                    <div className="product-info">
+                        <h3>{createProductName(bestSellingTv)}</h3>
+                        <p className="product-price">{createProductPrice(bestSellingTv.price)}</p>
+                        <p>{createScreenSizesString(bestSellingTv.availableSizes)}</p>
+                        <ul className="option-list">
+                            <li><img src={check} alt="Icoon: aanwezig" className="icon"/>wifi</li>
+                            <li><img src={minus} alt="Icoon: niet aanwezig" className="icon"/>speech</li>
+                            <li><img src={check} alt="Icoon: aanwezig" className="icon"/>hdr</li>
+                            <li><img src={check} alt="Icoon: aanwezig" className="icon"/>bluetooth</li>
+                            <li><img src={minus} alt="Icoon: niet aanwezig" className="icon"/>ambilight</li>
+                        </ul>
+                    </div>
+                </article>
             </section>
         </main>
     )
